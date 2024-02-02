62809c3a28c9c9138418c368a42e1e975d8c0e13
Sasha <sanykosovs@gmail.com>
2024-02-02 22:50:05 +0200

    homework-js

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let age;
+
+age = 13;
+
+console.log(age);
+
+console.log(name);
+var nme = 'Sasha';
+
+console.log(isStudent);
+
+'true';
+
+console.log(myString);
+
+'Робота не вовк, вмій і саночки возити'
+
+console.log(myNumber);
+let num = 3;
+
+
+{
+    let result = 13;
+}
+
+console.log(myNull); 
+
+const str = "nul";
