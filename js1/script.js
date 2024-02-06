39ae284bc61606685e09007082eedc7f16ab5a81
Sasha <sanykosovs@gmail.com>
2024-02-06 20:28:12 +0200

    homework-js2

---
+++ js1/script.js
@@ -0,0 +1,22 @@
+let age = 13;
+
+console.log(age);
+
+console.log(name);
+var name = 'Sasha';
+
+const isStudent = true;
+
+console.log(isStudent);
+
+let myString = 'Робота не вовк, в ліс не втече';
+
+console.log(myString);
+
+let myNumber = 3; 
+
+console.log(myNumber);
+
+const myNull = "nul";
+
+console.log(myNull);
---
+++ js2/script.js
@@ -0,0 +1,15 @@
+'use strict'
+
+//  date = promt('Як вас звати', 'Sasha');
+//  console.log(date)
+
+ 
+zaputani = confirm('Ви впевнені, що хочете продовжити?') {
+    alert("Дякую за підтвердження!");
+  } {
+    alert("Дію відмінено!");
+  }
+  
+  
+// nebezpeka = alert('action is dangerous');
+// console.log(nebezpeka)
