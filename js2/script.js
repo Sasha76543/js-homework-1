8814cf4814059ecdcdf260da3db9b491ebe51c81
Sasha <sanykosovs@gmail.com>
2024-02-09 15:09:17 +0200

    js2

--- js2/script.js
+++ js2/script.js
@@ -1,22 +1,22 @@
 'use strict'
 
-  var date = prompt('Як вас звати', 'Sasha');
-  hello = alert('Привіт Sasha');
+  // var date = prompt('Як вас звати', 'Sasha');
+  // hello = alert('Привіт Sasha');
 
-  console.log(date, hello);
+  // console.log(date, hello);
  
 //  if (confirm("Ви підтверджуєте дію")) {
 //    alert("Дякую за підтвердження!");
 // } else {
 //   alert("Дію відмінено!");
 //  }
-// danger = alert('Ця дія небезпечна!');
+danger = alert('Ця дія небезпечна!');
 
-// var approve = confirm('Підтверджуєте виконання дії?');
+var approve = confirm('Підтверджуєте виконання дії?');
 
-// if (approve) {
-//   alert('Дякую за підтвердження!');
-// } else {
-//   alert('Дію відмінено!');
-// }
-//   console.log(danger, approve);
+if (approve) {
+  alert('Дякую за підтвердження!');
+} else {
+  alert('Дію відмінено!');
+}
+  console.log(danger, approve);
