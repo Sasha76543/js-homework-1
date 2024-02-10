a9d922264ef318066163210e4bd9d06d2815d9af
Sasha <sanykosovs@gmail.com>
2024-02-10 11:32:56 +0200

    js3-homework

--- js3/script.js
+++ js3/script.js
@@ -1,17 +1,59 @@
 //temperature
+const celsiusTemperature = 25;
 
-//mans
+const fahrenheitTemperature = celsiusTemperature * 9/5 + 32;
+
+console.log(celsiusTemperature,fahrenheitTemperature);
+//Month
+
+let Monthdays = 31;
+
+let Monthhours = Monthdays * 24;
+
+let Monthminutes = Monthhours * 60;
+
+console.log(+ Monthdays + hoursInMonth + minutesInMonth );
 
 //game
 
-//buy
+let health = 100;
+let energy = 200;
 
-//okrugleni 
+health - 20;
+energy - 50;
+
+console.log(health);
+console.log(energy);
+
+
+//buy
+let suma = 1000
+
+suma = 1000 / 20%;
+//Rounding
+    number = 5.2;
+
+    numberrounding = Math.floor(number);
+
+console.log(numberrounding);
+
 
 //desytkove
+numeric = 8 / 3;
+
+numericdecimal = parseFloat(numeric);
+
+console.log(numericdecimal);
 
 //cile
+let figure = 55;
 
+let wholefigure = parseInt(figure);
+
+console.log(wholefigure);
 //kvadrat
+let date = 36;
 
-//cilo chuselyne
+let rootdate = Math.sqrt(number);
+
+console.log(+ date + rootdate);
