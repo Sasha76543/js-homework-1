852ce8f55a601652aa0580a9a389fb48c3a85f13
Sasha <sanykosovs@gmail.com>
2024-02-17 19:27:27 +0200

    js5 homework

---
+++ js5/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let Name = "Саша Косовський";
+
+console.log(Name.length);
+
+let Namemy = "john";
+let UpperCasename = Namemy.toUpperCase();
+console.log(UpperCasename);
+
+const fullname = "Саша Косовський";
+
+const indexOFullname = fullname.indexOf("");
+console.log(indexOFullname);
