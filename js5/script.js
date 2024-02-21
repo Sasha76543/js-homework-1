bf17046804be2c8d08a26e601f18e02c6c8b9e45
Sasha <sanykosovs@gmail.com>
2024-02-21 16:28:16 +0200

    js5

--- js5/script.js
+++ js5/script.js
@@ -16,3 +16,20 @@ console.log(indexOFullname);
 let number = 5 + 5 + "5";
 console.log(number);
 console.log(typeof number);
+
+const email = "Sashakosovskiy95@gmail.com";
+console.log(email.replace("@"));
+console.log(email.lenght);
+
+let userName = "Sasha";
+let payment = "300";
+let message = `Дякуємо,${userName}!До сплати ${payment}гривень`;
+
+alert(message);
+
+const word1 = 'My';
+const word2 = 'name';
+const word3 = 'is';
+
+const name = `${word1} ${word2} ${word3} Victor`
+console.log(name);
