5548a18cce24d0085a1588ad954f6ac817691eb6
Sasha <sanykosovs@gmail.com>
2024-02-25 18:03:37 +0200

    js6

--- js6/script.js
+++ js6/script.js
@@ -7,7 +7,7 @@ link.setAttribute("src", "img/encore-ewp100sb-72405815397367.webp");
 //2
 const link = document.getElementById("link");
 
-link.setAttribute("href", "img/35-obmp-007-max-228.png");
+link.setAttribute("link", "img/35-obmp-007-max-228.png");
 
 const image = document.getElementById("img");
 
