a260e05245a93898399f23e2e743eab81dd9abe1
Sasha <sanykosovs@gmail.com>
2024-02-25 09:24:11 +0200

    js6

---
+++ js6/script.js
@@ -0,0 +1,14 @@
+"use strict";
+//1
+const src = document.getElementById("img");
+
+link.setAttribute("src", "img/encore-ewp100sb-72405815397367.webp");
+
+//2
+const link = document.getElementById("link");
+
+link.setAttribute("href", "img/35-obmp-007-max-228.png");
+
+const image = document.getElementById("img");
+
+image.setAttribute("alt", "prapor 35 obrmp");
