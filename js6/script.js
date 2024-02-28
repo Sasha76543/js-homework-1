5f36e0502a7a96e76b1b13f575891609610d45a2
Sasha <sanykosovs@gmail.com>
2024-02-28 19:43:47 +0200

    js6

--- js6/script.js
+++ js6/script.js
@@ -12,3 +12,23 @@ link.setAttribute("link", "img/35-obmp-007-max-228.png");
 const image = document.getElementById("img");
 
 image.setAttribute("alt", "prapor 35 obrmp");
+
+//3
+
+document.addEventListener("DOMContentLoaded", function() {
+    const btn = document.getElementById("btn");
+    const input = document.getElementById("input");
+
+    btn.addEventListener("click", function() {
+        btn.innerText = input.value;
+    });
+});
+
+//4
+
+const ul = document.getElementById('ul');
+
+const first = ul.querySelector('li');
+first.textContent = 'first item li';
+
+
