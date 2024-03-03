f7b90e81844d4f21635661085ce73733c61ffa4b
Sasha <sanykosovs@gmail.com>
2024-03-03 14:10:07 +0200

    js7

---
+++ js7/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// const addButton = document.getElementById("button");
+// const header = document.getElementById("text");
+
+// addButton.addEventListener("click", function () {
+//   header.classList.add("color");
+// });
+
+// function displayInput() {
+//   const input = document.getElementById("input").value;
+//   document.getElementById("p").innerText = "Твоя фраза: " + input;
+// }
+
+function changeColor() {
+  const btn = document.querySelector(".button");
+  btn.style.backgroundColor = "lime";
+}
